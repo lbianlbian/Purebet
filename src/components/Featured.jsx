a426a123644d48923ca31387f8125e3b044e1322
Aniket Bamotra <aniketbamotra@gmail.com>
2022-06-29 21:05:12 +0530

    getOdds Api working

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import axios from "axios";
 import "./component.scss";
 
 const Featured = () => {
-  const getOdds = (id2, ha) => {
+  const [odds, setOdds] = useState(null);
+  const [isSliderOpen, setisSliderOpen] = useState();
+  const [eventId, setEventId] = useState();
+
+  const toggleSlider = (id) => {
+    setisSliderOpen(true);
     axios
-      .get(`https://showodds.wesleybian.repl.co?id1=0&id2=${id2}&ha=${ha}`)
+      .get(
+        `https://script.google.com/macros/s/AKfycbwnHC3Apglmz8YADs5Iyw2_W2PbZL_7ytFunadNpBs2OPhMiG9Xyb3RvHnOoVXG0fUu/exec?id2=${id}`
+      )
       .then(function (response) {
         // handle success
-        console.log(response);
+        console.log(response.data.home);
+        var arr = response.data.home;
+        console.log(arr[1]);
+        setOdds(response.data);
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       });
+    setEventId(id - 1);
   };
+  const closeSlider = () => {
+    setisSliderOpen(false);
+    setOdds(null);
+  };
+
+  const events = [
+    { home: "George Washington", away: "Abraham Lincoln" },
+    { home: "100 Thieves", away: "FaZe Clan" },
+    { home: "USC Trojans", away: "Northwestern Wildcats" },
+    { home: "Betting Sims will Win", away: "Betting Sims will Lose" },
+    { home: "Los Angeles Lakers", away: "Chicago Bulls" },
+  ];
+
   return (
     <div className="event-container" id="sect-2">
       <h1 className="heading">Featured Events.</h1>
       <div className="events-wrap">
-        <div className="event-item" onClick={() => getOdds(1, 1)}>
+        <div className="event-item" onClick={() => toggleSlider(1)}>
           <StaticImage
             src="../images/Group 3.png"
             alt="event-image"
@@ -33,7 +57,7 @@ const Featured = () => {
             Abraham Lincoln
           </p>
         </div>
-        <div className="event-item">
+        <div className="event-item" onClick={() => toggleSlider(2)}>
           <StaticImage
             src="../images/Rectangle 9.png"
             alt="event-image"
@@ -46,7 +70,7 @@ const Featured = () => {
             FaZe Clan
           </p>
         </div>
-        <div className="event-item">
+        <div className="event-item" onClick={() => toggleSlider(3)}>
           <StaticImage
             src="../images/Rectangle 10.png"
             alt="event-image"
@@ -59,7 +83,7 @@ const Featured = () => {
             Northwestern Wildcats
           </p>
         </div>
-        <div className="event-item">
+        <div className="event-item" onClick={() => toggleSlider(4)}>
           <StaticImage
             src="../images/Rectangle 11.png"
             alt="event-image"
@@ -72,7 +96,7 @@ const Featured = () => {
             Betting Sims will Lose
           </p>
         </div>
-        <div className="event-item">
+        <div className="event-item" onClick={() => toggleSlider(5)}>
           <StaticImage
             src="../images/Group 4.png"
             alt="event-image"
@@ -86,12 +110,63 @@ const Featured = () => {
           </p>
         </div>
       </div>
-      <h1 className="heading">Odds are:</h1>
-      <ul>
-        <li className="body-text">Highest Odds:</li>
-        <li className="body-text">Average Odds:</li>
-        <li className="body-text">Lowest Odds:</li>
-      </ul>
+      <div
+        className={`odds-slider-wrapper ${
+          isSliderOpen && "odds-slider-wrapper-open"
+        }`}
+        onClick={closeSlider}
+      >
+        <div className={`odds-slider ${isSliderOpen && "odds-slider-open"}`}>
+          {odds ? (
+            <div className="slider-content">
+              <div className="row1">
+                <div className="heading">{events[eventId].home}</div>
+                <div className="ho">
+                  <ul className="first-cell">
+                    <li className="odds body-text">{odds.home[0].odds}</li>
+                    <li className="bet-avail body-text">234 SOL</li>
+                  </ul>
+                </div>
+                <div className="sho">
+                  <ul className="second-cell">
+                    <li className="odds body-text">{odds.home[1].odds}</li>
+                    <li className="bet-avail body-text">24 SOL</li>
+                  </ul>
+                </div>
+                <div className="tho">
+                  <ul className="third-cell">
+                    <li className="odds body-text">{odds.home[2].odds}</li>
+                    <li className="bet-avail body-text">114 SOL</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="row2">
+                <div className="heading">{events[eventId].away}</div>
+                <div className="ho">
+                  <ul className="first-cell">
+                    <li className="odds body-text">{odds.away[0].odds}</li>
+                    <li className="bet-avail body-text">234 SOL</li>
+                  </ul>
+                </div>
+                <div className="sho">
+                  <ul className="second-cell">
+                    <li className="odds body-text">{odds.away[1].odds}</li>
+                    <li className="bet-avail body-text">24 SOL</li>
+                  </ul>
+                </div>
+                <div className="tho">
+                  <ul className="third-cell">
+                    <li className="odds body-text">{odds.away[2].odds}</li>
+                    <li className="bet-avail body-text">114 SOL</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1 className="heading">Loading</h1>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
