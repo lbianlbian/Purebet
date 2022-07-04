3cc9d0e58d66ce1edc50ade244f1b552bea91f5c
Aniket Bamotra <aniketbamotra@gmail.com>
2022-07-04 14:27:50 +0530

    added deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
+  pathPrefix: "/Betting-Sims-",
   siteMetadata: {
     title: `Betting Sims.`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+  ],
 };
